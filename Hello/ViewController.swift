8cf035a0c44f38c80245f717eb7bbef67f708610
ravisharmaa <bastola.ravi@gmail.com>
2019-04-05 08:29:36 +0545

    Actions Lesson

--- Hello/ViewController.swift
+++ Hello/ViewController.swift
@@ -10,5 +10,36 @@ class ViewController: UIViewController {
     @IBAction func buttonTapped(_ sender: UIButton) {
         sender.setTitle("This is changed", for: .normal)
     }
+    
+    
+    @IBAction func switchTapped(_ sender: UISwitch) {
+        if sender.isOn {
+            print("I am on")
+        
+        } else {
+            print("dude offed me")
+        }
+    }
+    
+    
+    @IBAction func slidingAction(_ sender: UISlider) {
+        
+        if sender.value < 5 {
+            print("\(sender.value)")
+        }
+    }
+    
+    @IBAction func keyboardReturnKeyTapped(_ sender: UITextField) {
+        if let text = sender.text {
+            print(text)
+        }
+    }
+    
+    @IBAction func didChangeEditing(_ sender: UITextField) {
+        if let text = sender.text {
+            print(text)
+        }
+    }
+    
 }
 
