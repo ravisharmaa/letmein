b5d5ce9cddf165ac43bd86dedd08c99e6bbb007f
ravisharmaa <bastola.ravi@gmail.com>
2019-04-06 20:55:18 +0545

    Outlets practice

--- Hello/ViewController.swift
+++ Hello/ViewController.swift
@@ -2,11 +2,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textLabel: UILabel!
+    
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func setTextButtonTapped(_ sender: UIButton) {
+        textLabel.text = textField.text
+    }
+    
+    @IBAction func clearTextButtonTapped(_ sender: UIButton) {
+        textLabel.text = ""
+        textField.text = ""
+    }
 }
 
